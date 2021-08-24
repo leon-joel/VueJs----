c0335cf9b1df12dd767eb332557ca62c29d3adcd
Leon <leopard10000@gmail.com>
2021-08-24 21:40:16 +0900

    watch immediate trueの実験
    初回呼び出し時のみ createdで作ったdelayFuncが見えない問題あり。

--- watcher.js
+++ watcher.js
@@ -1,31 +1,36 @@
 new Vue({
     el: '#app',
     data: {
-        name: '',
-        // upperName: '',
+        name: 'test',
+        upperName: '',
     },
 
-    created: function(){
+    created: function () {
         // delayFuncが最後に呼び出されてから 2000ms 経過後に getUpper を呼び出す。
         // 但し、2000ms以内に再度呼び出されたら、タイマーを2000msにリセットする。
         this.delayFunc = _.debounce(this.getUpper, 2000);
     },
 
-    // watch: {
-    //     name: function(newValue, oldValue) {
-    //         // nameが変更されたら delayFunc を呼び出す
-    //         this.delayFunc();
-    //     }
-    // },
-    computed: {
-        upperName: function () {
-            return this.name.toUpperCase();
-        }
+    watch: {
+        name: {
+            handler: function (newValue, oldValue) {
+                // nameが変更されたら delayFunc を呼び出す
+                this.delayFunc();
+            },
+            deep: true,
+            // immediate: false,
+            immediate: true, // ←一応動作はするが、trueだと「delayFuncが関数じゃない」というエラーが1回だけ発生する。createdが走るタイミングの問題？
+        },
+        // computed: {
+        //     upperName: function () {
+        //         return this.name.toUpperCase();
+        //     }
+        // },
     },
 
     methods: {
         getUpper: function () {
             this.upperName = this.name.toUpperCase();
-        }
+        },
     }
 })
