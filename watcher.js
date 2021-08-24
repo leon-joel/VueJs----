92b70f76db40a883f92fbd12d8444febe79f1e86
Leon <leopard10000@gmail.com>
2021-08-24 20:58:31 +0900

    comment

--- watcher.js
+++ watcher.js
@@ -6,11 +6,14 @@ new Vue({
     },
 
     created: function(){
+        // delayFuncが最後に呼び出されてから 2000ms 経過後に getUpper を呼び出す。
+        // 但し、2000ms以内に再度呼び出されたら、タイマーを2000msにリセットする。
         this.delayFunc = _.debounce(this.getUpper, 2000);
     },
 
     watch: {
         name: function(newValue, oldValue) {
+            // nameが変更されたら delayFunc を呼び出す
             this.delayFunc();
         }
     },
