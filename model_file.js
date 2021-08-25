8f8ffd2871f97b3518a2fc6fa3855f7dbe380026
leo <leopard10000@gmail.com>
2021-08-25 23:14:59 +0900

    ファイルアップロードサンプル ※サーバーがいないので動作はしない

---
+++ model_file.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+      message: '',
+    },
+    methods: {
+        onChange: function () {
+            let that = this;
+            let fl = this.$refs.upfile.files[0];
+            let data = new FormData();
+            data.append('upfile', fl, fl.name);
+
+            fetch('upload.php', {
+                method: 'POST',
+                body: data,
+            })
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (text) {
+                that.message = text;
+            })
+            .catch(function (error) {
+                window.alert('Error: ' + error.message);
+            });
+        }
+    }
+});
