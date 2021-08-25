945c9bf0b4135f0461565b0f09810d3195916459
leo <leopard10000@gmail.com>
2021-08-26 08:44:20 +0900

    双方向バインディング: src⇔target 間でカスタム処理を入れる

---
+++ model_custom.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+      mails: ["a@test.com", "bbb@example.com", "ccc@t.co.jp", "山田@eee.jp", "今清水@ddd.uk"],
+    },
+    created: function () {
+    },
+    methods: {
+    }
+});
