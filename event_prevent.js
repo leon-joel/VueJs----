5a5ec2cd03322c6512f8b7b84690e740a98a77c2
Leon <leopard10000@gmail.com>
2021-08-27 08:02:55 +0900

    条件付のEventキャンセルは .prevent 修飾子では対応出来ないので、preventDefault()メソッドを使う

--- event_prevent.js
+++ event_prevent.js
@@ -1,10 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-      
+        email: "test@email.com"
     },
     created: function () {
     },
     methods: {
+        onSubmit: function (e) {
+            if (!confirm("OK?")) {
+                console.log("Sended!");
+                e.preventDefault();
+                return;
+            }
+            console.log("Sended!");
+        }
     }
 });
