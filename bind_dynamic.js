df597d133d304adc10c4c704628bd8a117724926
Leon <leopard10000@gmail.com>
2021-08-27 00:15:24 +0900

    v-bind:[attr] で属性値をBind

---
+++ bind_dynamic.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        image_attr: {
+            attr: "width",
+            size: 100,
+        },
+    },
+    created: function () {
+    },
+    methods: {
+    }
+});
